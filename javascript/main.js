9a67e21695deb1771729e3f82e2e0279474e5486
International <international.resource.help@gmail.com>
2016-03-14 19:35:43 -0400

    Fixed indentation

--- javascript/main.js
+++ javascript/main.js
@@ -4,24 +4,23 @@ and isComplete properties */
 
 var todos = []
 
-
 var newTodoInput = document.getElementById('new-todo-input')
 var todoList = document.getElementById('todo-list')
 var numOfTodos = document.getElementById('number-of-todos')
-var title = document.querySelector("#new-todo-input input[name=title]")
-var description = document.querySelector("#new-todo-input input[name=description]")
+var title = document.querySelector('#new-todo-input input[name=title]')
+var description = document.querySelector('#new-todo-input input[name=description]')
 
-function renderTodos() {
-  todoList.innerHTML = todos.map(function(todo, index) {
-    var string ='<li>' +
-    '<label title=' + todo.description + '>' +   todo.title + '</label>' +
-    '<input type="checkbox" class="checkbox" data-index="' + index + '">'+
+function renderTodos () {
+  todoList.innerHTML = todos.map(function (todo, index) {
+    var string = '<li>' +
+    '<label title=' + todo.description + '>' + todo.title + '</label>' +
+    '<input type="checkbox" class="checkbox" data-index="' + index + '">' +
     ' <button class="remove-todo" data-index="' + index + '">X</button>'
-    if (index === 0){
+    if (index === 0) {
       string += ' <button class="move-todo-down" data-index="' + index + '">&#8595</button>' +
       '</li>'
     } else if (index === todos.length - 1) {
-      string +=  ' <button class="move-todo-up" data-index="' + index + '">&#8593</button>' +
+      string += ' <button class="move-todo-up" data-index="' + index + '">&#8593</button>' +
       '</li>'
     } else {
       string += ' <button class="move-todo-up" data-index="' + index + '">&#8593</button>' +
@@ -32,17 +31,16 @@ function renderTodos() {
   }).join('')
 }
 
-function renderNumberOfTodos(){
+function renderNumberOfTodos () {
   numOfTodos.innerHTML = todos.length
 }
 
-
-function clearAll(){
+function clearAll () {
   todos = []
   renderAll()
 }
 
-function renderAll(){
+function renderAll () {
   renderTodos()
   renderNumberOfTodos()
   title.value = ''
@@ -50,33 +48,32 @@ function renderAll(){
   title.focus()
 }
 
-function randomTodo(){
-  var rand = todos[Math.floor(Math.random()*todos.length)].title
+function randomTodo () {
+  var rand = todos[Math.floor(Math.random() * todos.length)].title
   alert(rand)
   renderAll()
 }
 
-function isNotChecked(obj) {
-    return !obj.isComplete
+function isNotChecked (obj) {
+  return !obj.isComplete
 }
 
-function clearChecked() {
-    todos = todos.filter(isNotChecked)
-    renderAll()
+function clearChecked () {
+  todos = todos.filter(isNotChecked)
+  renderAll()
 }
-
 
 newTodoInput.onsubmit = function(event) {
   event.preventDefault()
-     todos.push({
-     title: title.value,
-     description: description.value,
-     isComplete: false
-   })
-   renderAll()
- }
+  todos.push({
+    title: title.value,
+    description: description.value,
+    isComplete: false
+  })
+  renderAll()
+}
 
-todoList.onclick = function(event) {
+todoList.onclick = function (event) {
   var clickedElement = event.target
   var idx = clickedElement.dataset.index
   if (clickedElement.className === 'remove-todo') {
@@ -85,14 +82,14 @@ todoList.onclick = function(event) {
   } else if (clickedElement.className === 'move-todo-up') {
     var temp = todos[idx]
     todos[idx] = todos[idx - 1]
-    todos[idx -1] = temp
+    todos[idx - 1] = temp
     renderAll()
   } else if (clickedElement.className === 'move-todo-down') {
     var temp = todos[idx]
     todos[idx] = todos[idx - (-1)]
     todos[idx - (-1)] = temp
     renderAll()
-  } else if (clickedElement.type ==='checkbox') {
+  } else if (clickedElement.type === 'checkbox') {
     todos[idx].isComplete = clickedElement.checked
   }
 }
